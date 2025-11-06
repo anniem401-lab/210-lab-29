ad59b3e75d36c2dac50372384535e0eb8fe1dadd
Annie <annmora21@gmail.com>
2025-11-05 21:41:43 -0800

    10 min update

--- l-29.cpp
+++ l-29.cpp
@@ -8,13 +8,17 @@
 #include <map>
 #include <list>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 // Including additional headers that may need to be addded
 using namespace std;
 
 // A function defined to simulate a factory changing each production cycle
 // Parameters: map of departments, number of intervals
 // Function prototype
-// Void function to simulate factory: void factory_sim(map);
+// Void function to simulate factory: 
+void factory_sim(map<string, array<list<string>, 3>> &factory, int interval);
 
 // Define main function
 // Initialize a map to store factory information stored in an array of lists for:
@@ -22,21 +26,36 @@ using namespace std;
 
 int main (){
     // main program 
+    srand(time(0));
+    map<string, array<list<string>, 3>> factory; // map of factory
 
     // Open an external file to read data about the facotry and popoulate the map
     // Opening file "factory_data.txt"...
     // If file doesn't open, output error and close out.
+    ifstream file("factory_data.txt");
+    if (!file){
+        cout << "Error opening file." << endl; // Outputs error
+        return 1; // Exits program
+    }
     // Else: Ingredients, cookies, and employees are read from each line.
 
     // Closing file...
+    file.close();
     // file closed
 
     // factory_sim(map)
 
+    cout << "**Factory Stats**\n" ;
+    cout << "Ingredients: ";
+    cout << "Cookies: ";
+    cout << "Employees: ";
+
     // Time-based simulation for factory starts
     // For 25 intervals, interate through each department in the map and simulate changes
-    // Random events impact the factory:
-    // Hired worker, new cookie batches, change in stock, cookies packed, etc...
+    factory_sim(factory, 25);
+    
+    // Random events that may impact the factory:
+    // Hired/fired worker, new cookie batches, change in stock, cookies packed, etc...
 
     // Wait or pause breifly to simulate the passage of time between intervals
 
