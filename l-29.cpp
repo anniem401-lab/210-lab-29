c438ba7a610e20cc87ac7ca3fb47dbe27c0ca13f
Annie <annmora21@gmail.com>
2025-11-05 20:56:15 -0800

    10 min update

--- l-29.cpp
+++ l-29.cpp
@@ -8,19 +8,28 @@
 #include <map>
 #include <list>
 #include <string>
+// Including additional headers that may need to be addded
 using namespace std;
 
 // A function defined to simulate a factory changing each production cycle
-    // Parameters: map of departments, number of intervals
+// Parameters: map of departments, number of intervals
+// Void function to simulate factory
 
 // Define main function
-    // Initialize a map to store factory information stored in an array of lists for:
-    // ingredients, cookie batches, and employees
+// Initialize a map to store factory information stored in an array of lists for:
+// ingredients, cookie batches, and employees
+
 int main (){
-    // main program 
-}
+// main program 
 
 // Open an external file to read data about the facotry and popoulate the map
-    // If file doesn't open, output error and close out.
+// If file doesn't open, output error and close out.
+// Else: Ingredients, cookies, and employees are read from each line.
 
 // Close file
+
+// Time-based simulation for factory starts
+// For 25 intervals, interate through each department in the map and simulate changes
+// Random events impact the factory:
+// Hired worker, new cookie batches, change in stock, cookies packed
+}
