01bfa0b90298e3cb9b89478382b0bda5cdc5f4a8
Annie <annmora21@gmail.com>
2025-11-05 21:14:49 -0800

    10 min update

--- l-29.cpp
+++ l-29.cpp
@@ -13,23 +13,38 @@ using namespace std;
 
 // A function defined to simulate a factory changing each production cycle
 // Parameters: map of departments, number of intervals
-// Void function to simulate factory
+// Function prototype
+// Void function to simulate factory: void factory_sim(map);
 
 // Define main function
 // Initialize a map to store factory information stored in an array of lists for:
 // ingredients, cookie batches, and employees
 
 int main (){
-// main program 
+    // main program 
 
-// Open an external file to read data about the facotry and popoulate the map
-// If file doesn't open, output error and close out.
-// Else: Ingredients, cookies, and employees are read from each line.
+    // Open an external file to read data about the facotry and popoulate the map
+    // Opening file "factory_data.txt"...
+    // If file doesn't open, output error and close out.
+    // Else: Ingredients, cookies, and employees are read from each line.
 
-// Closing file
+    // Closing file...
+    // file closed
 
-// Time-based simulation for factory starts
-// For 25 intervals, interate through each department in the map and simulate changes
-// Random events impact the factory:
-// Hired worker, new cookie batches, change in stock, cookies packed
+    // factory_sim(map)
+
+    // Time-based simulation for factory starts
+    // For 25 intervals, interate through each department in the map and simulate changes
+    // Random events impact the factory:
+    // Hired worker, new cookie batches, change in stock, cookies packed, etc...
+
+    // Wait or pause breifly to simulate the passage of time between intervals
+
+    // End of main function
 }
+
+// Void function definition to simulate factory
+/* void factory_sim(map){
+    // void program defining the factory 
+}
+*/
