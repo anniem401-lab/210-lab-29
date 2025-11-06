43ff8ebac7efddecad0d3aff5dee4a36938d3f86
Annie <annmora21@gmail.com>
2025-11-05 20:37:45 -0800

    10 min update

---
+++ l-29.cpp
@@ -0,0 +1,26 @@
+// COMSC 210 | Lab 29 | Annie Morales
+// IDE used: Visual Studio Code
+
+// Include necessary headers for file handling, map, array, list, data structures, etc.
+#include <array>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <list>
+#include <string>
+using namespace std;
+
+// A function defined to simulate a factory changing each production cycle
+    // Parameters: map of departments, number of intervals
+
+// Define main function
+    // Initialize a map to store factory information stored in an array of lists for:
+    // ingredients, cookie batches, and employees
+int main (){
+    // main program 
+}
+
+// Open an external file to read data about the facotry and popoulate the map
+    // If file doesn't open, output error and close out.
+
+// Close file
