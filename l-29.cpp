ec9dbedeb4a1fd76b5b25fa8117135f16e57f18a
Annie <annmora21@gmail.com>
2025-11-09 16:20:19 -0800

    update 4

--- l-29.cpp
+++ l-29.cpp
@@ -67,11 +67,11 @@ int main (){
     // Will out put three departments: baking, packaging, sales
     for (const auto &dept : factory){
         cout << "\nDepartment: " << dept.first << "\n";
-        cout << " Items: " << setw (12);
+        cout << " Items: " << setw (10);
         for (const auto &ing : dept.second[0]){
             cout << ing << " " ;
         }
-        cout << "\n Cookies: " << setw (17);
+        cout << "\n Cookies: " << setw (12);
         for (const auto &cook : dept.second[1]){
             cout << cook << " ";
         }
@@ -79,18 +79,19 @@ int main (){
         for (const auto &emp : dept.second[2]){
             cout << emp << " ";
         }
-        cout << "\n-------------------------\n";
+        cout << "\n---------------------------------------------\n";
     } //setw (5)
 
     // Time-based simulation for factory starts
     // For 25 intervals, interate through each department in the map and simulate changes
     factory_sim(factory, 25);
+    cout << "\n====================================================" << endl;
 
     
     // Random events that may impact the factory during simulation which are defined in factory_sim:
     // Hired/fired worker, new cookie batches, change in stock, cookies packed, etc...
     // New factory stats are outputted at the end of each interval.
-    cout << "\n\n** New Factory Stats **\n";
+    cout << "\n** New Factory Stats **\n";
     for (const auto &dept : factory){
         cout << "\nDepartment: " << dept.first << "\n";
         cout << "Cookies produced: " << dept.second[1].size() << "\n";
@@ -107,12 +108,13 @@ int main (){
 void factory_sim(map<string, array<list<string>, 3>> &factory, int interval){
     // void program defining the factory 
     // The random events occur in the factory and intervals are counted until 25 is reached.
-    cout << "\n\n** Simulating 25 Factory Cycles **";
+    cout << "\n====================================================" << endl;
+    cout << "\n** Simulating 25 Factory Cycles **";
     for (int i = 1; i <= interval; ++i){
         cout << "\n\n --- Factory Cycle " << i << " ---\n";
 
         for (auto &dept : factory){
-            int event = rand() % 4; // Picks a random event (0-3)
+            int event = rand() % 5; // Picks a random event (0-4)
 
             switch (event){
                 case 0: // New cookie batch produced
@@ -137,6 +139,14 @@ void factory_sim(map<string, array<list<string>, 3>> &factory, int interval){
                     cout << "An employee was let go in " << dept.first << " department.\n";
                 } else {
                     cout << dept.first << ": No employees to let go!\n";
+                }
+                    break;
+                case 4: // Packaging cookies
+                if (!dept.second[1].empty()){
+                    dept.second[1].pop_back();
+                    cout << "A cookie batch was packaged in " << dept.first << " department.\n";
+                } else {
+                    cout << dept.first << ": No cookie batches to package!\n";
                 }
                     break;
             }
